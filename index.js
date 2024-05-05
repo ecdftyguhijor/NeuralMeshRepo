235638633fc00590fec45fda0bfef72115d547b0
ecdftyguhijor <rsjcoberthelmly@hotmail.com>
2024-05-06 01:43:30 +0700

    aptos commit

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
-function combinationSum4(nums, target) {
-  const dp = new Array(target + 1).fill(0);
-  dp[0] = 1;
-  for (let i = 1; i <= target; i++) {
-    for (const num of nums) {
-      if (i >= num) {
-        dp[i] += dp[i - num];
-      }
-    }
+function findMin(nums) {
+  let left = 0;
+  let right = nums.length - 1;
+  while (left < right) {
+    const mid = Math.floor((left + right) / 2);
+    if (nums[mid] > nums[right]) left = mid + 1;
+    else right = mid;
   }
-  return dp[target];
+  return nums[left];
 }
