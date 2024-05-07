48d28a0de3b7c809fb912eff4a5f533251cb954e
ecdftyguhijor <rsjcoberthelmly@hotmail.com>
2024-05-07 10:21:43 +0700

    feat project commit

--- index.js
+++ index.js
@@ -1,19 +1,16 @@
-function uniquePathsWithObstacles(obstacleGrid) {
-  const m = obstacleGrid.length;
-  const n = obstacleGrid[0].length;
-  const dp = new Array(m).fill(0).map(() => new Array(n).fill(0));
-  if (obstacleGrid[0][0] === 1) return 0;
-  dp[0][0] = 1;
-  for (let i = 1; i < m; i++) {
-    if (obstacleGrid[i][0] === 0) dp[i][0] = dp[i - 1][0];
+function minPathSum(grid) {
+  const rows = grid.length;
+  const cols = grid[0].length;
+  for (let i = 1; i < rows; i++) {
+    grid[i][0] += grid[i - 1][0];
   }
-  for (let j = 1; j < n; j++) {
-    if (obstacleGrid[0][j] === 0) dp[0][j] = dp[0][j - 1];
+  for (let j = 1; j < cols; j++) {
+    grid[0][j] += grid[0][j - 1];
   }
-  for (let i = 1; i < m; i++) {
-    for (let j = 1; j < n; j++) {
-      if (obstacleGrid[i][j] === 0) dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+  for (let i = 1; i < rows; i++) {
+    for (let j = 1; j < cols; j++) {
+      grid[i][j] += Math.min(grid[i - 1][j], grid[i][j - 1]);
     }
   }
-  return dp[m - 1][n - 1];
+  return grid[rows - 1][cols - 1];
 }
