c0ad2a6157f3fec828138e390ee9d509e734955f
ecdftyguhijor <rsjcoberthelmly@hotmail.com>
2024-05-08 07:47:06 +0700

    update project commit

--- index.js
+++ index.js
@@ -1,16 +1,28 @@
-function minPathSum(grid) {
-  const rows = grid.length;
-  const cols = grid[0].length;
-  for (let i = 1; i < rows; i++) {
-    grid[i][0] += grid[i - 1][0];
-  }
-  for (let j = 1; j < cols; j++) {
-    grid[0][j] += grid[0][j - 1];
-  }
-  for (let i = 1; i < rows; i++) {
-    for (let j = 1; j < cols; j++) {
-      grid[i][j] += Math.min(grid[i - 1][j], grid[i][j - 1]);
+function numIslands(grid) {
+  if (grid.length === 0) return 0;
+  let count = 0;
+  for (let i = 0; i < grid.length; i++) {
+    for (let j = 0; j < grid[0].length; j++) {
+      if (grid[i][j] === "1") {
+        dfs(grid, i, j);
+        count++;
+      }
     }
   }
-  return grid[rows - 1][cols - 1];
+  return count;
+}
+function dfs(grid, i, j) {
+  if (
+    i < 0 ||
+    i >= grid.length ||
+    j < 0 ||
+    j >= grid[0].length ||
+    grid[i][j] === "0"
+  )
+    return;
+  grid[i][j] = "0";
+  dfs(grid, i + 1, j);
+  dfs(grid, i - 1, j);
+  dfs(grid, i, j + 1);
+  dfs(grid, i, j - 1);
 }
